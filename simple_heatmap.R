8b0ee915b4bf2eb4315c800f4b4bff085c5ed7ad
Neha Sahu <110894234+nehasahu486@users.noreply.github.com>
2022-11-29 09:43:43 +0100

    Update simple_heatmap.R

--- simple_heatmap.R
+++ simple_heatmap.R
@@ -1,10 +1,10 @@
 library(gplots)
 
 ##Load the file with expression data (fpkm/rpkm/tpm) for which you want to create a heatmap
-exp_data <- read.delim("fpkm.tsv", row.names = 1)
+exp_data<-read.delim("fpkm.tsv", row.names = 1)
 
 ##Convert your data into a matrix, so gplots can process it
-exp_matrix<- data.matrix(exp_data)
+exp_matrix<-data.matrix(exp_data)
 
 ##check few lines of your matrix just to see if things look right
 head(exp_matrix)
@@ -14,15 +14,15 @@ head(exp_matrix)
 mycols = colorRampPalette(c("blue","white","red"))(100) 
 
 ##set dendrogram clustering method
-hr <- hclust(as.dist(1-cor(t(exp_matrix), method="pearson")), method="average") 
+hr<-hclust(as.dist(1-cor(t(exp_matrix), method="pearson")), method="average") 
 
-htmap <- heatmap.2(exp_matrix, Rowv= as.dendrogram(hr), dendrogram = "row", trace = "none",  
+htmap<-heatmap.2(exp_matrix, Rowv= as.dendrogram(hr), dendrogram = "row", trace = "none",  
                col = mycols, keysize = 1, key.title = "Title", Colv = FALSE,
                scale = c("row"), cexRow = 0.75, cexCol = 1, srtCol = NULL,  
                srtRow = NULL, adjRow = c(0,NA), sepcolor="white" ,margins = c(7,15), 
                 lwid = c(2,8),
                lhei= c(2,8), main ="Title", na.color="black") #this is the final one for the heat map
 
-#you can change the red text to "column" or "both" for whichever dendrogram you require
-##for long names in the y-axis then can change the margins
+#you can change the dendrogram to "column" or "both" as per your requirement
+##for long names in the x- or y-axis then can change the margins
 
